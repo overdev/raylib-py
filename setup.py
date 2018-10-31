54efb51147e10b6fdf08432c45998d69f3874f11
Jorge A. Gomes <jorgegomes83@hotmail.com>
2018-10-31 21:50:54 -0200

    Updated package version (to v0.1.1).

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',  # Required
+    version='0.1.1',  # Required
 
     python_requires='>=3.3, <4',
 
